9c667bdffe6a257db6555f636c3f02a3c1592841
Speechless22 <61347609+Speechless22@users.noreply.github.com>
2022-07-28 19:50:56 +0800

    重整

--- Keil uVision/C51/4-1/build/Debug/builder.mk
+++
@@ -1,5 +0,0 @@
-CFLAGS := PRINT(.\build\Debug\${c}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\..\..\..\..\..\Program Files\Keil_v5\C51\INC;.\src) DEFINE(__UVISION_VERSION='526')
-CXXFLAGS := PRINT(.\build\Debug\${cpp}.lst) OMF2 TABS(4) CODE NOCOND SYMBOLS OPTIMIZE(8,SPEED) SMALL ROM(LARGE) INCDIR(.\..\..\..\..\..\..\Program Files\Keil_v5\C51\INC;.\src) DEFINE(__UVISION_VERSION='526')
-ASMFLAGS := PRINT(.\build\Debug\${asm}.lst) SET(SMALL) INCDIR(.\..\..\..\..\..\..\Program Files\Keil_v5\C51\INC;.\src)
-LDFLAGS :=  PRINT(.\build\Debug\4-1.map) REMOVEUNUSED
-LDLIBS := 
